832ac4cf0bdd7cae657ed870b515d404503cc591
hiteshv <hiteshvijay7@gmail.com>
2020-07-18 16:19:12 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const fs = require('fs')
+var cors = require('cors')
+const app = express()
+
+app.use(cors({optionSuccessStatus: 200}))
+app.use(express.static('public'))
+
+
+app.get('/featured', (req, res) => {
+   const jsonPath = __dirname + '/data/featuredWalls.json'
+   res.sendFile(jsonPath)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/walls.html') 
+})
+
+const port = 3000
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
